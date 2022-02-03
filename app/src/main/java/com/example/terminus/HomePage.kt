b0a5761fd02af7c114ab02c7cfea503fb4ca325b
ScrufffScrufff <50392747+ScrufffScrufff@users.noreply.github.com>
2022-02-02 21:55:40 -0600

    switched the homescreens around and changed layout of main

--- app/src/main/java/com/example/terminus/HomePage.kt
+++ app/src/main/java/com/example/terminus/HomePage.kt
@@ -1,12 +1,11 @@
 package com.example.terminus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.content.Intent
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
-
 
 class HomePage : AppCompatActivity() {
     var nextActivityButton : Button? = null
@@ -16,15 +15,13 @@ class HomePage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        nextActivityButton = findViewById<View>(R.id.button4) as Button
-        question1 = findViewById<View>(R.id.textView3) as TextView
+        nextActivityButton = findViewById<View>(R.id.button) as Button
+        question1 = findViewById<View>(R.id.textView2) as TextView
         question1!!.text = "ASDF".trimIndent()
 
         nextActivityButton!!.setOnClickListener {
             val intent = Intent(this@HomePage, MainActivity::class.java)
             startActivity(intent)
         }
-
-
     }
 }
--- app/src/main/java/com/example/terminus/MainActivity.kt
+++ app/src/main/java/com/example/terminus/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.terminus
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.content.Intent
+import android.view.View
 import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     var nextActivityButton : Button? = null
@@ -15,13 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nextActivityButton = findViewById<View>(R.id.button) as Button
-        question1 = findViewById<View>(R.id.textView2) as TextView
+        nextActivityButton = findViewById<View>(R.id.button4) as Button
+        question1 = findViewById<View>(R.id.textView3) as TextView
         question1!!.text = "ASDF".trimIndent()
 
         nextActivityButton!!.setOnClickListener {
             val intent = Intent(this@MainActivity, HomePage::class.java)
             startActivity(intent)
         }
+
+
     }
 }
