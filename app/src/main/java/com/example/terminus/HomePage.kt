619b10a08d608e34f46885ffbf4538ef34b2623d
Trisha.Johnson <SPTXJ0@c02zw6zqmd6r.target.com>
2022-02-02 21:11:40 -0600

    linked two screens

--- app/src/main/java/com/example/terminus/HomePage.kt
+++ app/src/main/java/com/example/terminus/HomePage.kt
@@ -1,27 +1,30 @@
 package com.example.terminus
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.content.Intent
+import android.view.View
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+
+class HomePage : AppCompatActivity() {
     var nextActivityButton : Button? = null
     var question1: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
 
-        nextActivityButton = findViewById<View>(R.id.button) as Button
-        question1 = findViewById<View>(R.id.textView2) as TextView
+        nextActivityButton = findViewById<View>(R.id.button4) as Button
+        question1 = findViewById<View>(R.id.textView3) as TextView
         question1!!.text = "ASDF".trimIndent()
 
         nextActivityButton!!.setOnClickListener {
-            val intent = Intent(this@MainActivity, HomePage::class.java)
+            val intent = Intent(this@HomePage, MainActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
