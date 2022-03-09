9874ff342d7179a1e06d7ab47366316de580ba56
Trisha.Johnson <SPTXJ0@c02zw6zqmd6r.target.com>
2022-03-09 17:14:43 -0600

    check for valid word after 4 letters

--- app/src/main/java/com/example/terminus/MainActivity.kt
+++ app/src/main/java/com/example/terminus/MainActivity.kt
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.terminus.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
     //top level variable using in binding that will be used across methods
     //lateinit is a promise that the variable will be initialized before using it
     lateinit var binding: ActivityMainBinding
+
+    //TODO larger list of valid words or an api call
+    val validWords = listOf<String>("trade", "juice", "horse")
+    var outcome: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +25,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //add a click listener to the submit button
-        binding.submitButton.setOnClickListener{ addToWord() }
+        binding.submitButton.setOnClickListener { addToWord() }
 
         //add a click listener to the challenge button
-        binding.challengeButton.setOnClickListener{ checkWord()}
+        binding.challengeButton.setOnClickListener { checkWord() }
     }
 
     fun addToWord() {
@@ -31,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         val existingWord = binding.word.text.toString()
 
         //clear out the prior outcome when starting a new word
-        if(existingWord == "") {
+        if (existingWord == "") {
             binding.outcome.text = null
         }
 
@@ -46,20 +51,42 @@ class MainActivity : AppCompatActivity() {
 
         //clear out the existing letter after it's appended
         binding.letter.text = null
-}
+
+        //determine if the word entered is at least 5 characters and a word
+        if (newWord.length > 4) {
+            checkWordDuring(newWord)
+        }
+    }
+
+    fun checkWordDuring(word: String) {
+        if (word in validWords) {
+            outcome = "$word is a valid word!  You lose!"
+
+            //show outcome
+            binding.outcome.text = outcome
+        }
+        //clear out existing word
+        binding.word.text = null
+    }
+
     fun checkWord() {
+
         //get the word that is being challenged
         val wordToCheck = binding.word.text.toString()
 
-        //TODO larger list of valid words or an api call
-        val validWords = listOf<String>("jest", "lest", "mess")
 
         //check if the word being challenged is in the list of valid words
-        val outcome : String
-        if (wordToCheck in validWords) {
-            outcome = "$wordToCheck is a valid word!"
-        } else
-            outcome = "$wordToCheck is not a valid word!"
+        val outcome: String
+
+        //if word being challenged is at least 5 letters, determine if it's in the valid words
+        if (wordToCheck.length < 4) {
+            outcome = "$wordToCheck is too short!"
+        } else {
+            if (wordToCheck in validWords) {
+                outcome = "$wordToCheck is a valid word!"
+            } else
+                outcome = "$wordToCheck is not a valid word!"
+        }
 
         //show the outcome
         binding.outcome.text = outcome
