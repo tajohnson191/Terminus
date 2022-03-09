4d1dc3b48b4a4cbc170bf18ca3b601ad3ed4c32b
Trisha.Johnson <SPTXJ0@c02zw6zqmd6r.target.com>
2022-03-09 15:39:46 -0600

    updated buttons

--- app/src/main/java/com/example/terminus/MainActivity.kt
+++ app/src/main/java/com/example/terminus/MainActivity.kt
@@ -1,27 +1,57 @@
 package com.example.terminus
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import com.example.terminus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var nextActivityButton : Button? = null
-    var question1: TextView? = null
+
+    //top level variable using in binding that will be used across methods
+    //lateinit is a promise that the variable will be initialized before using it
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        nextActivityButton = findViewById<View>(R.id.button) as Button
-        question1 = findViewById<View>(R.id.textView2) as TextView
-        question1!!.text = "ASDF".trimIndent()
+        //initializes the binding object which is used to access Views in the layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        nextActivityButton!!.setOnClickListener {
-            val intent = Intent(this@MainActivity, HomePage::class.java)
-            startActivity(intent)
-        }
+        //sets the contentView of the activity, instead of passing the resource ID. Specifies the root of the hierarchy of your views
+        setContentView(binding.root)
+
+        //add a click listener to the submit button
+        binding.submitButton.setOnClickListener{ addToWord() }
+
+        //add a click listener to the challenge button
+        binding.challengeButton.setOnClickListener{ checkWord()}
+    }
+
+    fun addToWord() {
+        //get the letter from the letter input field.
+        val stringInTextField = binding.letter.text.toString()
+
+        //get the existing word
+        val existingWord = binding.word.text.toString()
+
+        //append the new letter to the existing word
+        var newWord = StringBuilder(existingWord).append(stringInTextField).toString()
+
+        //show the new word
+        binding.word.text = newWord
+
+        //clear out the existing letter after it's appended
+        binding.letter.text = null
+}
+    fun checkWord() {
+        val wordToCheck = binding.word.text.toString()
+
+        val validWords = listOf<String>("jest", "lest", "mess")
+
+        if (wordToCheck in validWords) {
+            println("Valid word!")
+        } else
+            println("Not valid word!")
+
+
     }
 }
